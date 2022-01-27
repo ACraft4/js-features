90d7efdcf849b516b9b591af20913e78a9857bf2
Arthur Kravchenko <arturkra92@gmail.com>
2022-01-27 20:54:17 +0200

    update sub js and add bootstrap lib

--- src/js/subscription.js
+++ src/js/subscription.js
@@ -1,1 +1,20 @@
-console.log('Hey sub'); 
+const promt_delay = 1000; 
+const max_promt_attemts = 3; 
+
+let promtCounter = 0;
+let hasSubcribed = false; 
+
+
+const intervalId = setInterval(() => {
+    if (promtCounter === max_promt_attemts) {
+        console.log('stop!');
+        clearInterval(intervalId);
+        return;
+    }
+
+    console.log('Subscribe - ' + Date.now());
+    promtCounter += 1;
+}, promt_delay); 
+
+console.log()
+
