6999e7a5ab33a61b46c029c8532a419da76be970
Arthur Kravchenko <arturkra92@gmail.com>
2022-01-31 16:30:13 +0200

    add subscription js

--- src/js/subscription.js
+++ src/js/subscription.js
@@ -1,18 +1,46 @@
+// Надоедалка 
+import BSN from 'bootstrap.native';
+const modal = new BSN.Modal('#subscription-modal'); 
+
+const refs = {
+    modal: document.querySelector('#subscription-modal'),
+    subscribeBtn: document.querySelector('button[data-subscribe]'),
+};
+
 const promt_delay = 1000; 
 const max_promt_attemts = 3; 
-
 let promtCounter = 0;
-let hasSubcribed = false; 
+let hasSubscribed = false; 
 
+openModal();
 
-const intervalId = setInterval(() => {
-    if (promtCounter === max_promt_attemts) {
-        console.log('stop!');
-        clearInterval(intervalId);
+refs.modal.addEventListener('hide.bs.modal', openModal); 
+refs.subscribeBtn.addEventListener('click', onSubBtnClick);
+
+function openModal() {
+    if (promtCounter === max_promt_attemts || hasSubscribed) {
         return;
     }
+    setTimeout(() => {
+        modal.show();
+        promtCounter += 1; 
+    }, promt_delay);
+}
 
-    console.log('Subscribe - ' + Date.now());
-    promtCounter += 1;
-}, promt_delay); 
+function onSubBtnClick() {
+    hasSubscribed = true; 
+    modal.hide();
+}
+
+
+// const intervalId = setInterval(() => {
+//     if (promtCounter === max_promt_attemts) {
+//         console.log('stop!');
+//         clearInterval(intervalId);
+//         return;
+//     }
+
+//     console.log('Subscribe - ' + Date.now());
+//     promtCounter += 1;
+// }, promt_delay); 
 
